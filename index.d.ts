dd0eb3da5585533fdacd5acc909273b49ab948b8
Dmitrii Demenev <dmitrii@searchless.ca>
2025-09-22 23:29:06 -0600

    Added references to ensure that module declarations work properly

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
+/// <reference path="extensions/markers/Markers.d.ts" />
+/// <reference path="extensions/promotion-dialog/PromotionDialog.d.ts" />
+/// <reference path="extensions/accessibility/Accessibility.d.ts" />
+/// <reference path="extensions/right-click-annotator/RightClickAnnotator.d.ts" />
+
 import { COLOR } from "./view/ChessboardView";
 
 /**
