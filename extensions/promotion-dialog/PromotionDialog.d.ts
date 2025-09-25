456f01a8050df6da9c0467ab8c06bf1f35aa285e
Dmitrii Demenev <dmitrii@searchless.ca>
2025-09-24 20:26:41 -0600

    Added typing for the promotion dialog extension

--- extensions/promotion-dialog/PromotionDialog.d.ts
+++ extensions/promotion-dialog/PromotionDialog.d.ts
@@ -1,10 +1,34 @@
+type Piece = import("../../cm-chessboard").Piece;
+type Square = import("../../cm-chessboard").Square;
+type Color = import("../../view/ChessboardView").Color;
+
 declare module "cm-chessboard/src/extensions/promotion-dialog/PromotionDialog.js" {
     export class PromotionDialog {
         [key: string]: any;
     }
 
-    const PROMOTION_DIALOG_RESULT_TYPE: {
+    export type PromotionDialogExtraChessboardProps = {
+        showPromotionDialog: (
+            square: Square,
+            color: Color,
+            callback: (
+                promotionDialogResult: PromotionDialogResult
+            ) => void,
+        ) => void;
+        isPromotionDialogShown: () => boolean;
+    }
+
+    export type PromotionDialogExtension = import("../../cm-chessboard").Extension<PromotionDialogExtraChessboardProps>;
+
+    export const PROMOTION_DIALOG_RESULT_TYPE: {
         pieceSelected: "pieceSelected",
         canceled: "canceled"
     };
+
+    export type PromotionDialogResult = {
+        type: typeof PROMOTION_DIALOG_RESULT_TYPE.pieceSelected;
+        piece: Piece;
+    } | {
+        type: typeof PROMOTION_DIALOG_RESULT_TYPE.canceled;
+    }
 }
