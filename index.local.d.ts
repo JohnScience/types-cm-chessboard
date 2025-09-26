6828e838e18ee1d1d02cae2d96aac445b78a5f83
Dmitrii Demenev <dmitrii@searchless.ca>
2025-09-26 17:51:02 -0600

    Fixed the ambient module declaration in cm-chessboard.d.ts

--- index.local.d.ts
+++ index.local.d.ts
@@ -1,6 +1,5 @@
+/// <reference path="cm-chessboard.ambient.d.ts" />
 /// <reference path="extensions/markers/Markers.ambient.d.ts" />
 /// <reference path="extensions/promotion-dialog/PromotionDialog.ambient.d.ts" />
 /// <reference path="extensions/accessibility/Accessibility.ambient.d.ts" />
 /// <reference path="extensions/right-click-annotator/RightClickAnnotator.ambient.d.ts" />
-
-export * from "./cm-chessboard.local";
